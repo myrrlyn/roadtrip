7aa33e0c00f7d4137896dbf28bc4eaee818706b8
myrrlyn <self@myrrlyn.dev>
2021-09-02 23:51:23 -0400

    Allow the server to be accessed by the local network

--- config/dev.exs
+++ config/dev.exs
@@ -18,7 +18,7 @@ config :roadtrip, Roadtrip.Repo,
 config :roadtrip_web, RoadtripWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: 7387],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
